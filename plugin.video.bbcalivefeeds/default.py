2cb0c0cffe804ed62443c16a62bad2b9c96ee6e4
liquid8d <liquid8d@gmail.com>
2014-03-06 16:35:12 -0500

    BBCALiveFeeds 1.0.3 update
    Fixed urls.. hopefully

--- plugin.video.bbcalivefeeds/default.py
+++ plugin.video.bbcalivefeeds/default.py
@@ -1,4 +1,5 @@
 import urllib,urllib2,re,xbmcplugin,xbmcgui
+import json
 
 def INDEX(url):
         req = urllib2.Request(url)
@@ -37,9 +38,43 @@ def addLink(name,url,iconimage):
 
 #addLink('Feeds', 'http://ichunk.livestream.com/212.54/livestreamiphone/bigbrothercanada4-secretlocationdev000020130304-182952-high_1756/playlist.m3u8?wowzasessionid=133706173', 'DefaultVideo.png')
 
-addLink('Camera1', 'rtsp://212-125.livestream.com:8080/livestreamiphone/6388794_1191932_b9c17034_1_150@156550', 'DefaultVideo.png')
-addLink('Camera2', 'rtsp://213-171.livestream.com:8080/livestreamiphone/6388794_1191932_fa9ad33e_1_150@156550', 'DefaultVideo.png')
-addLink('Camera3', 'rtsp://212-133.livestream.com:8080/livestreamiphone/6388794_1191932_a1a15f22_1_150@156550', 'DefaultVideo.png')
-addLink('Camera4', 'rtsp://212-125.livestream.com:8080/livestreamiphone/6388794_1191932_5dc86cf7_1_150@156550', 'DefaultVideo.png')
+#get info for camera
+#http://new.livestream.com/api/accounts/6388794/events/2701984/viewing_info
+
+def find_between( s, first, last ):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def download(url, ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:14.0) Gecko/20100101 Firefox/14.0.1'):
+    try:
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', ua)
+        response = urllib2.urlopen(req)
+        data=response.read()
+        response.close()
+        return data
+    except:
+        logger.error('Error downloading: ' + url)
+    return None
+
+print('BBCALiveFeeds: Getting camera info')
+cam_event_ids = ['2596592', '2701973', '2701980', '2701984', '2712276']
+count = 0
+for x in cam_event_ids:
+    try:
+        count += 1
+        data = download('http://new.livestream.com/api/accounts/6388794/events/' + x + '/viewing_info')
+        m3u8 = str(json.loads(data)['streamInfo']['m3u8_url'])
+        id = find_between(m3u8, '/i/', '_1@')
+        link = 'http://player.livestream-f.akamaihd.net/i/' + id + '_1@156550/master.m3u8'
+        #mobile = 'rtsp://212-125.livestream.com:8080/livestreamiphone/' + id + '_1_150@156550'
+        addLink('Camera ' + str(count), link, 'DefaultVideo.png')
+        #addLink('Camera ' + str(count) + ' (mobile alt)', mobile, 'DefaultVideo.png')
+    except:
+        print('error retrieving for camera ' + str(count));
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
