b74dfca4dac7d8202ce7702577a312b6639be3a4
liquid8d <liquid8d@gmail.com>
2014-03-06 03:26:45 -0500

    Update bbcalivefeeds to 1.0.2
    initial support for BBCA 2014

--- plugin.video.bbcalivefeeds/default.py
+++ plugin.video.bbcalivefeeds/default.py
@@ -1,11 +1,5 @@
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 
-#http://xbigbrothercanada1x.api.channel.livestream.com/3.0/playlist.m3u8
-#http://xbigbrothercanada2x.api.channel.livestream.com/3.0/playlist.m3u8
-#http://xbigbrothercanada3x.api.channel.livestream.com/3.0/playlist.m3u8
-#http://xbigbrothercanada4x.api.channel.livestream.com/3.0/playlist.m3u8
-
-
 def INDEX(url):
         req = urllib2.Request(url)
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
@@ -43,13 +37,9 @@ def addLink(name,url,iconimage):
 
 #addLink('Feeds', 'http://ichunk.livestream.com/212.54/livestreamiphone/bigbrothercanada4-secretlocationdev000020130304-182952-high_1756/playlist.m3u8?wowzasessionid=133706173', 'DefaultVideo.png')
 
-addLink('Camera1', 'http://xbigbrothercanada1x.api.channel.livestream.com/3.0/playlist.m3u8', 'DefaultVideo.png')
-addLink('Camera1 ALT', 'rtsp://mobilestr.livestream.com/livestreamiphone/bigbrothercanada1', 'DefaultVideo.png')
-addLink('Camera2', 'http://xbigbrothercanada2x.api.channel.livestream.com/3.0/playlist.m3u8', 'DefaultVideo.png')
-addLink('Camera2 ALT', 'rtsp://mobilestr.livestream.com/livestreamiphone/bigbrothercanada2', 'DefaultVideo.png')
-addLink('Camera3', 'http://xbigbrothercanada3x.api.channel.livestream.com/3.0/playlist.m3u8', 'DefaultVideo.png')
-addLink('Camera3 ALT', 'rtsp://mobilestr.livestream.com/livestreamiphone/bigbrothercanada3', 'DefaultVideo.png')
-addLink('Camera4', 'http://xbigbrothercanada4x.api.channel.livestream.com/3.0/playlist.m3u8', 'DefaultVideo.png')
-addLink('Camera4 ALT', 'rtsp://mobilestr.livestream.com/livestreamiphone/bigbrothercanada4', 'DefaultVideo.png')
+addLink('Camera1', 'rtsp://212-125.livestream.com:8080/livestreamiphone/6388794_1191932_b9c17034_1_150@156550', 'DefaultVideo.png')
+addLink('Camera2', 'rtsp://213-171.livestream.com:8080/livestreamiphone/6388794_1191932_fa9ad33e_1_150@156550', 'DefaultVideo.png')
+addLink('Camera3', 'rtsp://212-133.livestream.com:8080/livestreamiphone/6388794_1191932_a1a15f22_1_150@156550', 'DefaultVideo.png')
+addLink('Camera4', 'rtsp://212-125.livestream.com:8080/livestreamiphone/6388794_1191932_5dc86cf7_1_150@156550', 'DefaultVideo.png')
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
